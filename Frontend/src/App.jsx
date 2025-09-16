c7ca2ccc163ca642f8f588628cbacab20c13808b
Tzuriel Yeffet <tzuriel66ye@yahoo.com>
2025-09-16 15:21:15 +0300

    refactor project tree

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,20 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import HomePage from "./pages/HomePage";
+import HomePage from "./pages/HomePage/HomePage";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
+import MainNavbar from "./components/mainNavbar/mainNavbar";
+import Footer from "./components/footer/footer";
 
 function App() {
   return (
     <>
+      <MainNavbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
       </Routes>
+      <Footer />
     </>
   );
 }
