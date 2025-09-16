30e392c5b602f92b5e8369486ac107343010b175
GiantbYTES <schakedamir@gmail.com>
2025-09-16 10:54:57 +0300

    add login/signup buttons to navbar, fix navbar position

--- tripwise/Frontend/tripwise-app/src/components/mainNavbar.jsx
+++ tripwise/Frontend/tripwise-app/src/components/mainNavbar.jsx
@@ -3,7 +3,10 @@ import tripwiseLogo from "../assets/tripwise_logo_new.png";
 
 function MainNavbar() {
   return (
-    <header className="p-3 mb-3 border-bottom">
+    <header
+      className="sticky-top p-3 mb-3 border-bottom"
+      style={{ top: 0, zIndex: 1020, position: "sticky" }}
+    >
       <div className="container">
         <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
           <a
@@ -40,58 +43,18 @@ function MainNavbar() {
               </a>
             </li>
           </ul>
-          <form
-            className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3"
-            role="search"
-          >
-            <input
-              type="search"
-              className="form-control"
-              placeholder="Search..."
-              aria-label="Search"
-            />
-          </form>
-          <div className="dropdown text-end">
-            <a
-              href="#"
-              className="d-block link-body-emphasis text-decoration-none dropdown-toggle"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
-            >
-              <img
-                src="https://github.com/mdo.png"
-                alt="mdo"
-                width="32"
-                height="32"
-                className="rounded-circle"
-              />
-            </a>
-            <ul className="dropdown-menu text-small">
-              <li>
-                <a className="dropdown-item" href="#">
-                  New project...
-                </a>
-              </li>
-              <li>
-                <a className="dropdown-item" href="#">
-                  Settings
-                </a>
-              </li>
-              <li>
-                <a className="dropdown-item" href="#">
-                  Profile
-                </a>
-              </li>
-              <li>
-                <hr className="dropdown-divider" />
-              </li>
-              <li>
-                <a className="dropdown-item" href="#">
-                  Sign out
-                </a>
-              </li>
-            </ul>
-          </div>
+          <ul className="nav">
+            <li className="nav-item">
+              <a href="#" className="nav-link link-body-emphasis px-2">
+                Login
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="#" className="nav-link link-body-emphasis px-2">
+                Sign up
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
     </header>
