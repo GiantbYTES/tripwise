d4d387ce34e78f9358bf2b7322238ee50d23fed1
Tzuriel Yeffet <tzuriel66ye@yahoo.com>
2025-09-18 09:32:54 +0300

    crash

--- Frontend/src/components/auth/AuthButton/AuthButton.jsx
+++ Frontend/src/components/auth/AuthButton/AuthButton.jsx
@@ -1,10 +1,10 @@
 import "./AuthButton.css"
-export function AuthButton({text,disabled,loading}){
+export function AuthButton({text,disabled,loading,onClick}){
 
 
     return (
         <>
-            <button className="auth-btn" disabled={disabled || loading}>
+            <button className="auth-btn" disabled={disabled || loading} onClick={onClick}>
                 {loading? "Loading...":text}
             </button>
         </>
--- Frontend/src/pages/auth/SignupPage/SignupPage.jsx
+++ Frontend/src/pages/auth/SignupPage/SignupPage.jsx
@@ -1,5 +1,4 @@
-
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { AuthForm } from "../../../components/auth/AuthForm/AuthForm.jsx";
 import { EmailField } from "../../../components/auth/EmailField/EmailField.jsx";
 import { PasswordField } from "../../../components/auth/PasswordField/PasswordField.jsx";
@@ -7,23 +6,40 @@ import { ConfirmPasswordField } from "../../../components/auth/ConfirmPasswordFi
 import { AuthButton } from "../../../components/auth/AuthButton/AuthButton.jsx";
 import "./SignupPage.css";
 export function SignupPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [isEmailValid, setIsEmailValid] = useState(false);
+  const [isPasswordValid, setIsPasswordValid] = useState(false);
+  const isMatch = password === confirmPassword && confirmPassword.length > 0;
+  const isValid = isEmailValid && isPasswordValid && isMatch;
 
-    const [email,setEmail] = useState("")
-    const [password,setPassword] = useState("")
-    const [loading,setLoading] = useState(false)
-    const [confirmPassword,setConfirmPassword] = useState("")
-    const [isEmailValid,setIsEmailValid] = useState(false)
-    const [isPasswordValid,setIsPasswordValid]= useState(false)
-    const isMatch = password === confirmPassword && confirmPassword.length>0
-    const isValid = isEmailValid && isPasswordValid && isMatch
+  console.log("isValid? ", isValid);
 
-    console.log("isValid? ",isValid);
-    
-    const handleSignup=()=>{
-
+  const handleSignup = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    const response = await fetch("http://localhost:3000/auth/signup", {
+      method: "POST",
+      headers: {
+        "Comtent-Type": "application/json",
+      },
+      body: JSON.stringify(email, password),
+    });
+    if (!response.ok) {
+      alert("Signup failed");
+      setLoading(false);
+      throw new Error("Signup failed");
     }
+    const data = await response.json();
 
-
+    localStorage.setItem("token", data.token);
+    localStorage.setItem("user", data.user);
+    setLoading(false);
+    alert("Signup Successful");
+    //redirecting to dashboard
+  };
 
   return (
     <>
@@ -31,10 +47,27 @@ export function SignupPage() {
         <div className="signup-form">
           <h1> Sign Up</h1>
           <AuthForm>
-            <EmailField value={email} onChange={setEmail} onValidChange={setIsEmailValid}/>
-            <PasswordField value={password} onChange={setPassword} onValidChange={setIsPasswordValid}/>
-            <ConfirmPasswordField password={password} value={confirmPassword} onChange={setConfirmPassword} />
-            <AuthButton text="Sing Up" disabled={!isValid}/>
+            <EmailField
+              value={email}
+              onChange={setEmail}
+              onValidChange={setIsEmailValid}
+            />
+            <PasswordField
+              value={password}
+              onChange={setPassword}
+              onValidChange={setIsPasswordValid}
+            />
+            <ConfirmPasswordField
+              password={password}
+              value={confirmPassword}
+              onChange={setConfirmPassword}
+            />
+            <AuthButton
+              text="Sign Up"
+              disabled={!isValid}
+              loading={loading}
+              onClick={handleSignup}
+            />
           </AuthForm>
         </div>
       </div>
