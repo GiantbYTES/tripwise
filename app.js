19601717dd9e371daa67b2f1f2cb2b9ff01d92c3
Tzuriel Yeffet <tzuriel66ye@yahoo.com>
2025-09-18 15:28:37 +0300

    add migration file for password column

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(cors({
   origin: "http://localhost:5173", 
-  credentials: true           ./BackEnd/routes/geminiRouter.jsns in headers
+  credentials: true          
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
