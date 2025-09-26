cb5ce025d6aa4affc2024e4de6e4d969a0d96c27
GiantbYTES <schakedamir@gmail.com>
2025-09-27 01:00:10 +0300

    change logout button style in main navbar

--- Frontend/src/components/auth/LogoutButton/LogoutButton.jsx
+++ Frontend/src/components/auth/LogoutButton/LogoutButton.jsx
@@ -2,21 +2,24 @@
 import { useAuth } from "../../../context/AuthContext.jsx";
 import { useNavigate } from "react-router-dom";
 export function LogoutButton() {
-  const { logout } = useAuth(); 
-  const navigate = useNavigate()
+  const { logout } = useAuth();
+  const navigate = useNavigate();
 
   const handleLogout = async () => {
     try {
       await logout();
-    navigate("/", { replace: true });
-
+      navigate("/", { replace: true });
     } catch (err) {
       console.error("Logout failed:", err);
     }
   };
 
   return (
-    <button className="btn btn-outline-primary" onClick={handleLogout}>
+    <button
+      className="nav-link link-body-emphasis px-2"
+      onClick={handleLogout}
+      style={{ background: "none", border: "none", cursor: "pointer" }}
+    >
       Logout
     </button>
   );
